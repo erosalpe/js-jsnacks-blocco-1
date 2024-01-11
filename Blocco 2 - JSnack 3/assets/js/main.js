78666375712b25008041c8d1b1d04c2ce10a41b8
erosalpe <eros.alpegiani@gmail.com>
2024-01-11 14:31:17 +0100

    feat(Blocco 2 JSnack 3) completato

---
+++ Blocco 2 - JSnack 3/assets/js/main.js
@@ -0,0 +1,15 @@
+let x = prompt("inserisci quanti numeri vuoi vengano inseriti nell'Array");
+let xNum = parseInt(x);
+
+let numeriInteriArray = [];
+let risultatoSommaDispari = 0;
+
+for (let i = 0; i <= xNum; i++){
+    numeriInteriArray[i] = i;
+    if (numeriInteriArray.length % 2 === 0){
+        console.log(`${risultatoSommaDispari}`, "+",  `${numeriInteriArray[i]}`);
+        risultatoSommaDispari = risultatoSommaDispari + numeriInteriArray[i];
+        console.log(`${risultatoSommaDispari}` );
+    }
+}
+document.writeln(`La somma dei numeri in posizione dispari è: ${risultatoSommaDispari}`)
