c77127050efaf7dd8750a0f2d2de00e57d56e75b
erosalpe <eros.alpegiani@gmail.com>
2024-01-12 10:01:44 +0100

    fix(blocco 1 JSnack 6)modificata elevazione al cubo prima era il quadrato

--- Blocco 1 - JSnack 6/assets/js/main.js
+++ Blocco 1 - JSnack 6/assets/js/main.js
@@ -2,5 +2,5 @@ let x = prompt("Inserisci un numero");
 let xNum = parseInt(x);
 
 for(let i = 0; i <= xNum; i++){
-    document.writeln(`Il cubo del numero ${i} è: ${i**2} <br>`);
+    document.writeln(`Il cubo del numero ${i} è: ${i**3} <br>`);
 }
